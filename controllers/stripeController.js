272e5f9c18899ea045a2612118c946b80f1a1bc8
FosterSOAsare <fostersoasare@gmail.com>
2024-09-13 00:23:02 +0000

    Stripe integration

---
+++ controllers/stripeController.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const stripe = require("stripe")(process.env.stripe_key);
+
+const generateCheckoutSession = async (req, res) => {
+	try {
+		const session = await stripe.checkout.sessions.create({
+			mode: "payment",
+			payment_method_types: ["card"],
+			line_items: [
+				{
+					price: process.env.priceID,
+					quantity: 1,
+				},
+			],
+			success_url: `${process.env.CLIENT_URL}/pricing/success`,
+			cancel_url: `${process.env.CLIENT_URL}/pricing/error`,
+		});
+
+		console.log(session);
+
+		// Store session.id so that it can be retrieved later for verification
+
+		res.json({ url: session.url });
+	} catch (e) {
+		res.status(500).json({ success: false, message: e.message });
+	}
+};
+
+const validateCheckoutSession = async (req, res) => {
+	// retrieve the session.id
+	const { sessionId } = req.query;
+	const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+	if (session.payment_status === "paid") {
+		// Update as needed
+	} else {
+		res.status(400).json({ error: "The session was not completed since user didn't make payment" });
+	}
+	res.status(200).json({ message: "Validation was successful" });
+};
+
+module.exports = {
+	generateCheckoutSession,
+	validateCheckoutSession,
+};
--- router.js
+++ router.js
@@ -1,39 +1,42 @@
-const express            = require('express');
-const attendeeController = require('./controllers/attendeeController.js');
-const eventController    = require('./controllers/eventController.js');
-const gamesController    = require('./controllers/gamesController.js');
-const devController      = require('./controllers/devcontroller.js');
+const express = require("express");
+const attendeeController = require("./controllers/attendeeController.js");
+const eventController = require("./controllers/eventController.js");
+const gamesController = require("./controllers/gamesController.js");
+const devController = require("./controllers/devcontroller.js");
+const { generateCheckoutSession } = require("./controllers/stripeController.js");
 
 const router = express.Router();
 
 // attendee endpoints
-router.get('/api/attendee/search', attendeeController.serchAttendee);
-router.post('/api/attendee/register', attendeeController.register);
-router.post('/api/attendee/addNote', attendeeController.addNote);
+router.get("/api/attendee/search", attendeeController.serchAttendee);
+router.post("/api/attendee/register", attendeeController.register);
+router.post("/api/attendee/addNote", attendeeController.addNote);
 
 // event endpoints
-router.get('/api/event/list', eventController.getEventList);
-router.get('/api/event/entryList', eventController.getEntryList);
-router.post('/api/event/create', eventController.createEvent);
-router.post('/api/event/join', eventController.joinEvent);
-router.delete('/api/event/cancel', eventController.cancelEvent);
-router.delete('/api/event/drop', eventController.dropFromEvent);
+router.get("/api/event/list", eventController.getEventList);
+router.get("/api/event/entryList", eventController.getEntryList);
+router.post("/api/event/create", eventController.createEvent);
+router.post("/api/event/join", eventController.joinEvent);
+router.delete("/api/event/cancel", eventController.cancelEvent);
+router.delete("/api/event/drop", eventController.dropFromEvent);
 
 // game library endpoints
-router.get('/api/gamelib/title/list', gamesController.getTitleList);
-router.get('/api/gamelib/copy/list', gamesController.getCopyList);
-router.get('/api/gamelib/title/checkouts', gamesController.getCheckoutsByTitle);
-router.post('/api/gamelib/title/create', gamesController.addTitle);
-router.post('/api/gamelib/copy/create', gamesController.addCopy);
-router.post('/api/gamelib/copy/checkout', gamesController.checkoutCopy);
-router.patch('/api/gamelib/title/edit', gamesController.editTitle);
-router.patch('/api/gamelib/copy/return', gamesController.returnCopy);
-router.patch('/api/gamelib/copy/addNote', gamesController.addNote);
-router.delete('/api/gamelib/title/remove', gamesController.removeTitle);
-router.delete('/api/gamelib/copy/remove', gamesController.removeCopy);
-
+router.get("/api/gamelib/title/list", gamesController.getTitleList);
+router.get("/api/gamelib/copy/list", gamesController.getCopyList);
+router.get("/api/gamelib/title/checkouts", gamesController.getCheckoutsByTitle);
+router.post("/api/gamelib/title/create", gamesController.addTitle);
+router.post("/api/gamelib/copy/create", gamesController.addCopy);
+router.post("/api/gamelib/copy/checkout", gamesController.checkoutCopy);
+router.patch("/api/gamelib/title/edit", gamesController.editTitle);
+router.patch("/api/gamelib/copy/return", gamesController.returnCopy);
+router.patch("/api/gamelib/copy/addNote", gamesController.addNote);
+router.delete("/api/gamelib/title/remove", gamesController.removeTitle);
+router.delete("/api/gamelib/copy/remove", gamesController.removeCopy);
 
 // dev endpoints
 // router.patch('/dev/editTable')
+
+// stripe endpoints
+router.get("/api/stripe/create-checkout-session", generateCheckoutSession);
 
 module.exports = router;
