d8dbf43060ab6832a6ac8fef3e6d6c4cee865f92
FosterSOAsare <fostersoasare@gmail.com>
2024-09-13 00:25:04 +0000

    Test purposes

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
-const { Pool, Client } = require('pg');
-const connectionString = 'postgresql://postgres@localhost:5432/lfg_con';
-const pool = new Pool({
-  connectionString,
-});
+// const { Pool, Client } = require('pg');
+// const connectionString = 'postgresql://postgres@localhost:5432/lfg_con';
+// const pool = new Pool({
+//   connectionString,
+// });
 
-const client = new Client({
-  connectionString,
-});
+// const client = new Client({
+//   connectionString,
+// });
 
-client.connect()
-module.exports = client;
+// client.connect()
+module.exports = {};
